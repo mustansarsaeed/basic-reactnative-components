dfed9d852bcf8be03686d7cd39c70684f43d6cbf
Mustansar Saeed <m.mustansar.saeed@gmail.com>
2022-09-15 19:09:58 +0500

    - Implemented different screens e.g., welcome and view image screens

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,45 @@
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import colors from "../config/colors";
+
+export default function WelcomeScreen() {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>Sell, whatever you need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    alignItems: "center",
+    position: "absolute",
+    top: 70,
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
